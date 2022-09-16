dc43ae5da407282ddfe5528601a0373516a458f2
nunobmoura <nunobaratamoura@gmail.com>
2022-09-16 20:50:53 +0100

    ExpenseListFilters test

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 // import { setTextFilter } from './actions/filters';
 // import getVisibleExpenses from './selectors/expenses';
 

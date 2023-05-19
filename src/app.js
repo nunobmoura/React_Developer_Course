254c9537e9c7b524ee9d0278697ce7e6c1019e2b
nunobmoura <nunobaratamoura@gmail.com>
2023-05-19 20:13:12 +0100

    clean app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-// import { setTextFilter } from './actions/filters';
-// import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -15,14 +10,6 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', createdAt: 1662030000000, amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1662030000000, amount: 200 }));
-store.dispatch(addExpense({ description: 'Rent', createdAt: 1662030000000, amount: 109500 }));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,42 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react'
-          ],
-          plugins: [
-            '@babel/plugin-proposal-class-properties'
-          ]
-        }
-      }
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  resolve: {
-    alias: {
-      Components: path.resolve(__dirname, 'src/components/'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+  return {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
     },
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        Components: path.resolve(__dirname, 'src/components/'),
+      },
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
 };
